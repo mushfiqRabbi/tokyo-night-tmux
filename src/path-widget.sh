9184456766d56572ea0b5faa5de6aad956186d0b
mushfiq <mushfiqxrabbi@gmail.com>
2025-04-26 06:36:02 +0600

    style(tmux): improve window status visibility and consistency

--- src/path-widget.sh
+++ src/path-widget.sh
@@ -23,4 +23,4 @@ if [[ ${PATH_FORMAT} == "relative" ]]; then
   current_path="$(echo ${current_path} | sed 's#'"$HOME"'#~#g')"
 fi
 
-echo "#[bg=default]${current_path} "
+echo "#[fg=${THEME[foreground]},bg=${THEME[background]}]${current_path}"
